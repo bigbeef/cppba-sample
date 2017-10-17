1702c0c66487f11d024f03caa55b086a7bfb70ff
winfed <245655812@qq.com>
2017-10-17 18:13:20 +0800

    spring-boot版本1.5.7

--- cppba-post/src/main/java/com/cppba/controller/PostController.java
+++ cppba-post/src/main/java/com/cppba/controller/PostController.java
@@ -1,10 +1,12 @@
 package com.cppba.controller;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @RestController
 public class PostController {
@@ -14,8 +16,9 @@ public class PostController {
     @ResponseBody
     //public String post(@RequestBody String people) { // post json
     //public String post(String people) { // post form
-    public String post(@RequestBody String body) {
-
+    public String post(String people,HttpServletRequest request) {
+        Map<String, String[]> params = request.getParameterMap();
+        System.out.println(12345);
         return "success";
     }
 }
